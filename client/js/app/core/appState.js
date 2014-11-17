93afb273fc5ec80f4883252d3822f705e163ad8c
Andrew Sygyda <andrew.sygyda@gmail.com>
2014-11-17 17:10:37 +0200

    Check age

--- client/js/app/core/appState.js
+++ client/js/app/core/appState.js
@@ -6,20 +6,7 @@ define(
     function (can, products) {
 
         var AppState = can.Map.extend({
-            define: {
-                // Is 18 years old was confirmed
-                is18Conf: {
-                    set: function (newVal) {
-                        if (newVal) {
-                            localStorage.setItem('is18Conf', newVal)
-                        }
-                        return newVal;
-                    },
-                    get: function () {
-                        return localStorage.getItem('is18Conf');
-                    }
-                }
-            },
+            is18Conf: false,
             products: products
         }),
         appState = new AppState();
